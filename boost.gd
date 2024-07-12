2176ee6ddf62454867980dfe3a1c47c55b88cc73
Aaron Murphy <126209301+Oakamer@users.noreply.github.com>
2024-07-12 22:11:42 +0100

    Update boost.gd

--- boost.gd
+++ boost.gd
@@ -1,9 +1,15 @@
 extends Area2D
 
+@onready var timer = $Timer
+
 
 func boost():
 	pass
 
 
 func _on_body_entered(body):
-	global.z = 500
+	global.z = 600
+	timer.start()
+	
+func _on_timer_timeout():
+	global.z = 400		
