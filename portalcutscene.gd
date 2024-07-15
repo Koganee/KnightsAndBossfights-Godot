a480165780416039baaa4023c50a43bf917f01c3
Aaron Murphy <126209301+Oakamer@users.noreply.github.com>
2024-07-15 14:24:44 +0100

    Update portalcutscene.gd

--- portalcutscene.gd
+++ portalcutscene.gd
@@ -1,6 +1,6 @@
 extends Control
 
-const loading_scene_path = "res://scenes//racinggame.tscn"
+const loading_scene_path = "res://scenes//kingdom_of_sweets.tscn"
 
 func _on_button_pressed():
 	get_tree().change_scene_to_file(loading_scene_path)
