d1c764b3111979cefd2849cabe1610e69a45b1fd
Aaron Murphy <126209301+Oakamer@users.noreply.github.com>
2024-07-17 11:39:34 +0100

    Update global.gd

--- global.gd
+++ global.gd
@@ -4,3 +4,11 @@ var hasShard = false
 var dialogue_started_switch_scene = true
 var z = 400
 var lapCount = 1
+var readyToRace = false
+
+#Rhythm Game
+var sensor_AL = 0
+var sensor_AD = 0
+var sensor_AU = 0
+var sensor_AR = 0
+#Rhythm Game
