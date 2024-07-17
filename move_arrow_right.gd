3a176da09e9f92a434a4e1bfd772e653f6ac27a3
Aaron Murphy <126209301+Oakamer@users.noreply.github.com>
2024-07-17 19:43:00 +0100

    Create move_arrow_right.gd

---
+++ move_arrow_right.gd
@@ -0,0 +1,22 @@
+extends Area2D
+
+var speed = 100
+var sensor = 0
+
+func _process(delta):
+	position.y -= speed * delta
+
+	if position.y < -50:
+		queue_free()
+	
+	if sensor == 1:
+		if global.sensor_AR == 1:
+			if Input.is_action_just_pressed("ui_right"):
+				queue_free()
+
+func _on_area_shape_entered(area_rid, area, area_shape_index, local_shape_index):
+	sensor = 1
+
+
+func _on_area_shape_exited(area_rid, area, area_shape_index, local_shape_index):
+	sensor = 0
