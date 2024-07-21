85766e6557a93cbabc88e25c1b28f56d4432fe47
Aaron Murphy <126209301+Oakamer@users.noreply.github.com>
2024-07-21 02:12:13 +0100

    Update idle_arrow_left.gd

--- idle_arrow_left.gd
+++ idle_arrow_left.gd
@@ -9,9 +9,11 @@ func _process(delta):
 	if sensor == 1:
 		if Input.is_action_just_pressed("move_left"):
 			animation_player.play("Good")
+			global.rhythmScore += 1
 	if sensor == 0:
 		if Input.is_action_just_pressed("move_left"):
-			animation_player.play("Bad")		
+			animation_player.play("Bad")	
+			global.rhythmLives -= 1
 	
 	
 
