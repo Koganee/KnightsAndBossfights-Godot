6a929ef6d66bfa1df385c4219dd11e945b506fad
Aaron Murphy <126209301+Oakamer@users.noreply.github.com>
2024-07-21 14:36:13 +0100

    Update bubble_seller.gd

--- bubble_seller.gd
+++ bubble_seller.gd
@@ -2,3 +2,9 @@ extends CharacterBody2D
 
 func bubbleSeller():
 	pass
+	
+
+func _process(delta):
+	if global.readyToRhythm == true:
+		get_tree().change_scene_to_file("res://scenes/rhythm_game.tscn")
+		global.readyToRhythm = false
